fece823d52030834587c293c82afa1de928e4ea5
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-20 13:36:14 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,9 +18,10 @@ def remove_strawberry(contacts)
   contacts.each do |name|
 
     name.each do |key, value|
-    if key == :favorite_ice_cream_flavors
-      if value[0] == "strawberry"
+      if key == :favorite_ice_cream_flavors
+        if value[0] == "strawberry"
         delete value[0]
+        end
       end
     end
   end
